61607f1d59155683fbc67b3a048aeae646eca279
DerexScript <Derex@outlook.com.br>
2020-03-28 04:18:05 -0300

    ...

--- commands/unban.js
+++ commands/unban.js
@@ -1,27 +1,24 @@
 const { Client, MessageEmbed } = require('discord.js');
 
-module.exports.run = async (bot, message, args) => {
-
+module.exports.run = async (client, message, args) => {
+    message.delete();
     if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])){
         return message.channel.send("Você não tem permissão para executar este comando!");   
     }
-
     if(isNaN(args[0])){
         return message.channel.send("Você precisa fornecer um ID.");
     } 
-
-    let bannedMember = await bot.fetchUser(args[0]);
-    
-    if(!bannedMember) return message.channel.send("Forneça um ID de usuário para desbanir alguém!")
-
-        let reason = args.slice(1).join(" ")
+    let bannedMember = await client.users.fetch(args[0]);
+    if(!bannedMember){
+        return message.channel.send("Forneça um ID de usuário para desbanir alguém!"); 
+    } 
+    let reason = args.slice(1).join(" ");
     if(!reason) reason = "Nenhuma razão dada!";
-
-    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("Você não tem permissão para executar este comando!")|
-        message.delete()
-
+    if(!message.guild.me.hasPermission("BAN_MEMBERS")){
+        return message.channel.send("Você não tem permissão para executar este comando!")    
+    }
     try {
-        message.guild.unban(bannedMember, reason)
+        message.guild.members.unban(bannedMember, reason)
         message.channel.send(`${bannedMember.tag} seu banimento foi revogado!`)
     } catch(e) {
         console.log(e.message)
@@ -35,5 +32,4 @@ module.exports.run = async (bot, message, args) => {
     .addField("Razão:", reason)
 
     message.channel.send(embed).catch(console.error);
-
 }
